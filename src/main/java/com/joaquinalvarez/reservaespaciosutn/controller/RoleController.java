4b82457070fa905298b7786b9e434b24dc712205
Joaquin Alvarez <alvarezdjoaquin@gmail.com>
2023-04-24 15:48:24 -0300

    Update controller and service from Role class

---
+++ src/main/java/com/joaquinalvarez/reservaespaciosutn/controller/RoleController.java
@@ -0,0 +1,27 @@
+package com.joaquinalvarez.reservaespaciosutn.controller;
+
+import com.joaquinalvarez.reservaespaciosutn.model.entity.Role;
+import com.joaquinalvarez.reservaespaciosutn.model.view.CreateRoleDto;
+import com.joaquinalvarez.reservaespaciosutn.service.RoleService;
+import com.joaquinalvarez.reservaespaciosutn.service.impl.RoleServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/role")
+public class RoleController {
+    @Autowired
+    private RoleService roleService;
+
+    @PostMapping("/add")
+    public ResponseEntity<Role> create(@RequestBody Role role){
+        return new ResponseEntity<>(roleService.save(role), HttpStatus.CREATED);
+    }
+}
---
+++ src/main/java/com/joaquinalvarez/reservaespaciosutn/service/RoleService.java
@@ -0,0 +1,14 @@
+package com.joaquinalvarez.reservaespaciosutn.service;
+
+import com.joaquinalvarez.reservaespaciosutn.model.entity.Role;
+import com.joaquinalvarez.reservaespaciosutn.model.view.CreateRoleDto;
+import com.joaquinalvarez.reservaespaciosutn.model.view.UpdateRolDto;
+
+public interface RoleService {
+
+    Role getById(Long id);
+    Role save(Role createRole);
+    void update(Long roleId, Role updateRoleDto);
+    void delete(Long roleId);
+
+}
