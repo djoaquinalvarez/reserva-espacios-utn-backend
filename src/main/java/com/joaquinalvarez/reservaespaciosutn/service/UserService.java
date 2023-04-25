50c68e53d0391a342e97d7f0473e948e26aa3e5b
Joaquin Alvarez <alvarezdjoaquin@gmail.com>
2023-04-25 10:06:50 -0300

    Add method PUT of UserService and UserController

--- src/main/java/com/joaquinalvarez/reservaespaciosutn/service/UserService.java
+++ src/main/java/com/joaquinalvarez/reservaespaciosutn/service/UserService.java
@@ -6,6 +6,6 @@ import com.joaquinalvarez.reservaespaciosutn.model.entity.User;
 public interface UserService {
     User getById(Long id);
     User save(User createUser);
-    void update(Long userId, User user);
+    User update(Long userId, User user);
     void delete(Long userId);
 }
