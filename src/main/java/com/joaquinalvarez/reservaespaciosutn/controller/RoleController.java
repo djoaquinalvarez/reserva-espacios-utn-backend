964bef24445e5d9cdf1e28895090099ba018376b
Joaquin Alvarez <dalvarez@mobydigital.com>
2023-04-29 12:28:12 -0300

    Implements GET method for Role

--- src/main/java/com/joaquinalvarez/reservaespaciosutn/controller/RoleController.java
+++ src/main/java/com/joaquinalvarez/reservaespaciosutn/controller/RoleController.java
@@ -12,12 +12,19 @@ import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 
 @RequestMapping("/role")
 public class RoleController {
     @Autowired
     private RoleService roleService;
+
+    @GetMapping("/get")
+    public ResponseEntity<List<Role>> getRoles(){
+        return new ResponseEntity<>(roleService.getRoles(), HttpStatus.OK);
+    }
 
     @PostMapping("/add")
     public ResponseEntity<Role> createRole(@RequestBody Role role){
--- src/main/java/com/joaquinalvarez/reservaespaciosutn/service/RoleService.java
+++ src/main/java/com/joaquinalvarez/reservaespaciosutn/service/RoleService.java
@@ -4,9 +4,12 @@ import com.joaquinalvarez.reservaespaciosutn.model.entity.Role;
 import com.joaquinalvarez.reservaespaciosutn.model.view.CreateRoleDto;
 import com.joaquinalvarez.reservaespaciosutn.model.view.UpdateRolDto;
 
+import java.util.List;
+
 public interface RoleService {
 
     Role getById(Long id);
+    List<Role> getRoles();
     Role save(Role createRole);
     Role update(Long roleId, Role updateRoleDto) throws Exception;
     void delete(Long roleId);
