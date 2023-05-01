e3620d6d96028d6a0b9b8dfedc1e9eea1b65ebeb
Joaquin Alvarez <dalvarez@mobydigital.com>
2023-05-01 14:17:41 -0300

    Add controller for Place

---
+++ src/main/java/com/joaquinalvarez/reservaespaciosutn/controller/PlaceController.java
@@ -0,0 +1,10 @@
+package com.joaquinalvarez.reservaespaciosutn.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/place")
+public class PlaceController {
+    
+}
