70b01898c3d4e43bb28c447e12160733ac83e3ed
Joaquin Alvarez <dalvarez@mobydigital.com>
2023-05-02 20:07:37 -0300

    Add validations of fields for Resource

--- src/main/java/com/joaquinalvarez/reservaespaciosutn/model/entity/Resource.java
+++ src/main/java/com/joaquinalvarez/reservaespaciosutn/model/entity/Resource.java
@@ -1,18 +1,23 @@
 package com.joaquinalvarez.reservaespaciosutn.model.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Entity
 @AllArgsConstructor
+@RequiredArgsConstructor
 @NoArgsConstructor
 @Data
 @Table(name = "resource")
 public class Resource {
 
     @Id
+    @NotNull
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
+    @NotNull
     private String name;
     private String description;
 
