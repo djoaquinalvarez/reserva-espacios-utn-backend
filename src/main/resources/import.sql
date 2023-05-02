3b6165fdb5e734d97aa12f95d27f152b65bc9bd5
Joaquin Alvarez <dalvarez@mobydigital.com>
2023-05-02 20:46:36 -0300

    Add file SQL for insert data initial

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO resource(id, name) VALUES(1, 'Proyector');
+INSERT INTO resource(id, name) VALUES(2, 'Pizarra');
+INSERT INTO resource(id, name) VALUES(3, 'Pantalla Televisor');
+INSERT INTO resource(id, name) VALUES(4, 'Pantalla Monitor');
+INSERT INTO resource(id, name) VALUES(5, 'Notebook');
