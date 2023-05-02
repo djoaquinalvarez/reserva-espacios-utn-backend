5060ba84ae122bf83cf86f869f0345db95598f0a
Joaquin Alvarez <dalvarez@mobydigital.com>
2023-05-02 19:22:04 -0300

    Add controller for Resource: implements GET method

---
+++ src/main/java/com/joaquinalvarez/reservaespaciosutn/controller/ResourceController.java
@@ -0,0 +1,25 @@
+package com.joaquinalvarez.reservaespaciosutn.controller;
+
+import com.joaquinalvarez.reservaespaciosutn.model.entity.Resource;
+import com.joaquinalvarez.reservaespaciosutn.service.ResourceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/resource")
+public class ResourceController {
+
+    @Autowired
+    private ResourceService resourceService;
+
+    @GetMapping("/get")
+    public ResponseEntity<List<Resource>> getResources(){
+        return new ResponseEntity<>(resourceService.getResources(), HttpStatus.OK);
+    }
+}
