94b105e623db86c3bd9276e23e263f3f98a7be8d
Zoy Eder <71857129+EderZoy@users.noreply.github.com>
2023-05-09 21:02:24 -0300

    Add GET method in StateController

--- src/main/java/com/joaquinalvarez/reservaespaciosutn/controller/StateController.java
+++ src/main/java/com/joaquinalvarez/reservaespaciosutn/controller/StateController.java
@@ -1,2 +1,25 @@
-package com.joaquinalvarez.reservaespaciosutn.controller;public class StateController {
+package com.joaquinalvarez.reservaespaciosutn.controller;
+
+import com.joaquinalvarez.reservaespaciosutn.model.entity.State;
+import com.joaquinalvarez.reservaespaciosutn.service.StateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/state")
+public class StateController {
+    @Autowired
+    private StateService stateService;
+    @GetMapping("/get")
+    public ResponseEntity<List<State>> getStates(){
+        return new ResponseEntity<>(stateService.findAll(), HttpStatus.OK);
+    }
+
+
 }
